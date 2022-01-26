8b7e5441f1d31a901648311e6edaa07c3611b4e5
vagrant <vagrant@case-env>
2022-01-26 21:09:43 +0000

    t

--- temperature_control/aadl/bin/run-hamr-SeL4.sh
+++ temperature_control/aadl/bin/run-hamr-SeL4.sh
@@ -4,24 +4,10 @@ SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 AADL_DIR=$SCRIPT_DIR/..
 ROOT_DIR=$SCRIPT_DIR/../..
 
-OSIREUM=osireum
-if [ -f "$1" ]; then
-  OSIREUM="$1 -nosplash -console -consoleLog -data @user.home/.sireum -application org.sireum.aadl.osate.cli"
-elif command -v $OSIREUM &> /dev/null ; then
-  OSIREUM=$OSIREUM
-elif [ -n "${OSIREUM_EXE}" ] && [ -f "${OSIREUM_EXE}" ]; then
-  OSIREUM="$OSIREUM_EXE -nosplash -console -consoleLog -data @user.home/.sireum -application org.sireum.aadl.osate.cli"
-else
-  echo "osireum not found.  Run '$SIREUM_HOME/bin/sireum hamr phantom -h' for instructions on"
-  echo "how to install the Sireum plugins into OSATE/FMIDE.  Then do one of the following:"
-  echo "  - Pass in the location of the OSATE/FMIDE executable that contains the Sireum plugins"
-  echo "  - Add the 'osireum' alias to your .bashrc file"
-  echo "  - Set the environment variable OSIREUM_EXE to point to the location of"
-  echo "      the OSATE/FMIDE executable that contains the Sireum plugins"
-  exit
-fi
+FMIDE=$SIREUM_HOME/bin/linux/fmide/fmide
+FMIDE_CLI="$FMIDE -nosplash -console -consoleLog -data @user.home/.sireum -application org.sireum.aadl.osate.cli"
 
-eval "$OSIREUM hamr codegen \
+eval "$FMIDE_CLI hamr codegen \
   --verbose \
   --platform seL4 \
   --package-name t \
